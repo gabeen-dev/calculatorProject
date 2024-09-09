17e2e3a0ad23e0cca2a3fb13092fde818f5e7bdb
gebeen-dev <rrdd1207@gmail.com>
2024-09-09 22:22:30 +0900

    계산기-class없는

---
+++ src/Main.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("찻번째 숫자를 입력하세요.(종료하려면 exit 입력)");
+            String first = sc.next();
+
+            if (first.equals("exit")) {
+                System.out.println("계산기를 종료합니다.");
+                break;
+            }
+
+            int i = Integer.parseInt(first);
+
+            System.out.println("연산기호를 입력하세요.");
+            String second = sc.next();
+
+            System.out.println("두번째 숫자를를 입력하세요.");
+            String third = sc.next();
+            int j = Integer.parseInt(third);
+
+        if (second.equals("+")) {
+            System.out.println("답: " + (i + j));
+        } else if (second.equals("-")) {
+            System.out.println("답: " + (i - j));
+        } else if (second.equals("*")) {
+            System.out.println("답: " + (i * j));
+        } else if (second.equals("/")) {
+            System.out.println("답: " + (i / j));
+            if (j != 0) {
+                System.out.println(i / j);
+            } else {
+                System.out.println("0으로 나눌수 없습니다");
+            }
+        } else {
+            System.out.println("잘못입력했습니다.");
+        }
+        }
+    }
+}
+
+
+
+
+
+
