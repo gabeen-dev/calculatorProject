ed90b035b2d62903da7e3e85619e2e3cd6a66097
gebeen-dev <rrdd1207@gmail.com>
2024-09-09 22:24:16 +0900

    with생성

--- src/without/WithoutMain.java
+++ src/without/WithoutMain.java
@@ -1,6 +1,8 @@
+package without;
+
 import java.util.Scanner;
 
-public class Main {
+public class WithoutMain {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
